ae3fa469e52daafab22bd66cacc503b820533eca
Varun <varunmukherjee7@gmail.com>
2023-06-01 18:54:17 +0530

    create prompt form added

--- src/app/create-prompt/page.tsx
+++ src/app/create-prompt/page.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
+"use client"
+
+import React,{useState} from 'react'
 
 import Form from '@/components/Form'
 
-const page:React.FC = () => {
+const CreatePrompt:React.FC = () => {
+
+  const [submitting,setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: ''
+  })
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <div>page</div>
+    <div>
+      <Form
+        type = "Create"
+        post = ""
+        setPost = ""
+        submitting = ""
+        handleSubmit = ""
+      />
+    </div>
   )
 }
 
-export default page
+export default CreatePrompt
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import React from 'react'
+import Link from 'next/link'
 
 interface Props {
     type: string,
@@ -9,14 +12,84 @@ interface Props {
 }
 
 const Form:React.FC<Props> = (props) => {
+
+  const handleSubmit = () => {
+
+  }
+
   return (
     <section className = "w-full max-w-full flex-start flex-col">
       <h1 className = "head_text text-left">
         <span className = "blue_gradient">{props.type} Post</span>
       </h1>
-      <p className = "">
-
+      <p className = "desc text-left max-w-md">
+        {props.type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered platform.
       </p>
+
+      <form
+        onSubmit = {handleSubmit}
+        className = "mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+
+        <label>
+          <span  className = "font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value = {props.post.prompt}
+            onChange = {(e) => {
+              props.setPost({
+                ...props.post,
+                prompt: e.target.value
+              })
+            }}
+            placeholder= "Write your prompt here ..."
+            required
+            className = "form_textarea"
+          />
+        </label>
+
+        <label>
+          <span  className = "font-satoshi font-semibold text-base text-gray-700">
+            Tag {' '} 
+            <span className = "font-normal">
+              (#product, #webdevelopment, #idea)
+            </span>
+          </span>
+
+          <input
+            value = {props.post.tag}
+            onChange = {(e) => {
+              props.setPost({
+                ...props.post,
+                tag: e.target.value
+              })
+            }}
+            placeholder= "#tag"
+            required
+            className = "form_input"
+          />
+        </label>
+
+        <div className = "flex-end mx-3 mb-5 gap-4">
+            <Link
+              href = "/"
+              className = "text-gray-500 text-sm"
+            >
+              Cancel
+            </Link>
+
+            <button
+              type = "submit"
+              disabled = {props.submitting}
+              className = "px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+            >
+              {props.submitting? `${props.type}...`:`${props.type}`}
+            </button>
+        </div>
+
+      </form>
     </section>
   )
 }
